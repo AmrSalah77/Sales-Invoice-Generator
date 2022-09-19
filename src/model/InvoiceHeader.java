880dd3f4ff6b0e0b89e5df5ea428f8c2bff4b7c9
AmrSalah <amrs22131@gmail.com>
2022-09-19 02:28:08 +0200

    implementation of model package

---
+++ src/model/InvoiceHeader.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Amr
+ */
+public class InvoiceHeader {
+     private int invoiceNum;
+     private Date invoiceDate;
+     private String customerName;
+     private ArrayList<InvoiceLine> lines;
+
+    public InvoiceHeader(int invoiceNum, Date invoiceDate, String customerName) {
+        this.invoiceNum = invoiceNum;
+        this.invoiceDate = invoiceDate;
+        this.customerName = customerName;
+    }
+
+    public int getInvoiceNum() {
+        return invoiceNum;
+    }
+
+    public void setInvoiceNum(int invoiceNum) {
+        this.invoiceNum = invoiceNum;
+    }
+
+    public Date getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(Date invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+     
+     
+}
